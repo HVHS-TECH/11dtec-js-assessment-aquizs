983f3848a0df67d12e4f09348e84936c04d66b26
aquizs <24157lw@hvhs.school.nz>
2026-06-25 13:30:23 +1200

    doing the homepage buttons stuf

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello world!")
+const OUTPUT = document.getElementById("JavaScriptOutput");
+
+function start (){
+    OUTPUT.innerHTML = "<h1>testing</h1>";
+}
+
+ function getFormInput(){
+    const NAME_FIELD = document.getElementById("nameField");
+    let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML = "<h1>Your name is "+userName+"</h1>";
+ }
