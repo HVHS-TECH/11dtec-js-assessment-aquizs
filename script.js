a24a35256c9041b45caa57e27922e16bccfdcb7f
aquizs <24157lw@hvhs.school.nz>
2026-07-21 18:42:25 +1200

    working on java script did some of the add to cart function still working on it

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 let itemNames = [];
 let itemPrices = [];
+let itemQuantity = [];
+
+function addToCart(name, price) {
+
+    let found = false;
+    for(let i=0; i<itemNames.length; i++) {
+        if(itemNames[i] == name) {
+            itemQantity[i] = itemQuantiy[i] + 1;
+            found = true;
+        }
+    }
+
+    if(found == false) {
+        itemNames.push(name);
+    }
+}
